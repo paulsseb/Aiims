7bc362ef424dbd873df4db010789be67536f9b39
Phillip256 <ssesphill@gmail.com>
2022-01-03 22:45:05 +0300

    modified signin index

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","inventory");
+$con = mysqli_connect("localhost","root","","");
 
 // Check connection
 if (mysqli_connect_errno())
--- dist/includes/db_con.php
+++ dist/includes/db_con.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Africa/Nairobi');
 $today = date('Y-m-d H:i:s A',time());
 try
 {
-    $dbh = new PDO('mysql:host=localhost; dbname=inventory', 'root', '');
+    $dbh = new PDO('mysql:host=localhost; dbname=aiims', 'root', '');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e)
